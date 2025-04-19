01a100f9d3e8abc10327215a26bed450a454c55f
InvestorChuks <85705774+InvestorChuks@users.noreply.github.com>
2025-04-20 00:39:48 +0100

    Update flask_runner.py

--- flask_runner.py
+++ flask_runner.py
@@ -14,19 +14,14 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 @app.route('/trading-view-alert', methods=['POST'])
 def trading_view_alert():
     try:
-        # Try to decode JSON data from the request
-        data = request.get_json(silent=True)  # silent=True prevents throwing an error
+        data = request.get_json(silent=True)
         if data is None:
-            # If no JSON data, treat as plain text
-            data = request.data.decode('utf-8')  # Decode bytes to string
+            data = request.data.decode('utf-8')
             message_text = data
         else:
-            # If JSON data, convert to formatted string
             message_text = json.dumps(data, indent=2)
 
         logging.info("Received alert data: %s", message_text)
-
-        # Send message to Telegram
         bot.send_message(chat_id=CHAT_ID, text=message_text)
         logging.info("Message sent to Telegram successfully")
 
@@ -42,5 +37,5 @@ def trading_view_alert():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # Running directly on port 80
+    app.run(debug=True, port=5000)
 
